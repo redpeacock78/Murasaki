6a9b36ba07d6deaae2c7606aab05a1a160cc123d
redpeacock78 <scarletpeacock78@gmail.com>
2021-09-23 08:29:00 +0900

    fix: Fix public/index.html

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@ export default {
   margin-top: 0;
   font-size: 18.5px;
   line-height: 1.7em;
-  font-family: "Noto Serif JP", HentaiKana, IPAmjm, "Noto Serif TC", serif !important;
+  font-family: "Noto Serif JP", "Noto Serif TC", HentaiKana, IPAmjm, serif !important;
   font-feature-settings: "palt", "trad";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
