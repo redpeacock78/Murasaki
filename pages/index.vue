d0b376c88ebdb09c54e8cc499329b1fbd2537ff4
redpeacock78 <scarletpeacock78@gmail.com>
2021-09-24 14:06:35 +0900

    fix: Change pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,6 @@ export default {
 <style lang="scss">
 * {
   margin-top: 0;
-  font-size: 18.5px;
   line-height: 1.7em;
   font-family: "Noto Serif JP", "Noto Serif TC", HentaiKana, IPAmjm, serif !important;
   font-feature-settings: "palt", "trad";
